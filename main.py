8c8f48bca5b38e93f1e7eb76f79493ac2a318452
Maksimov <georgmaximov160805@gmail.com>
2025-12-02 23:44:30 +0700

    feat: add matcher

---
+++ main.py
@@ -0,0 +1,31 @@
+import argparse
+
+from matcher.matcher import Matcher
+from gigachat_api.dependencies_prompt import _cached_prompt_manager
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("profile", type=str, help="User profile description.")
+    parser.add_argument("model_type", type=str, default="bert-l", help="Type for matcher model.")
+    parser.add_argument("matcher_threshold", type=str, default=0.5, help="Matcher threshold.")
+    args = parser.parse_args()
+
+    # parse vk
+    parsed_posts = None
+    
+    # call llm
+    llm_results = []
+    for parsed_post in parsed_posts:
+        if _cached_prompt_manager.send_text(parsed_post["text"]):
+            llm_results.append(parsed_post)
+
+    # match texts
+    matcher = Matcher(model_type=args.model_type,
+                      profile=args.profile,
+                      threshold=args.matcher_threshold,
+                      )
+    matcher(llm_results)
+
+
+if __name__ == "__main__":
+    main()
---
+++ matcher/matcher.py
@@ -0,0 +1,23 @@
+from base_predictor import BasePredictor
+from bert_predictor import BertPredictor
+
+class Matcher:
+    def __init__(self, model_type: str, profile: str, threshold=0.5):
+        self.predictor: BasePredictor | None = None
+        self.model_type = model_type
+        self.profile = profile
+        self.threshold = threshold
+
+    def _setup_predictor(self) -> None:
+        if self.model_type == "bert-l":
+            self.predictor = BertPredictor(self.model_type)
+        elif self.model_type == "word2vec-300":
+            raise NotImplementedError("Word2vec model type is not implemented")
+
+    def __call__(self, texts: list[tuple[int, str]]) -> dict[int, str]:
+        results = {}
+
+        for id, text in texts:
+            results[id] = self.predictor(self.profile, text) > self.threshold
+
+        return results
