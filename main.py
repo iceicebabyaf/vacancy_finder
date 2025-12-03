1496340589d74979d29cb0cbc1dc354238d50e6a
Maksimov <georgmaximov160805@gmail.com>
2025-12-04 00:24:50 +0700

    fix: main and requirements

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ from vk_parser.parser import analyze_vk_posts
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("profile", type=str, help="User profile description.")
-    parser.add_argument("model_type", type=str, default="bert-l", help="Type for matcher model.")
-    parser.add_argument("matcher_threshold", type=str, default=0.5, help="Matcher threshold.")
+    parser.add_argument("--profile", type=str, help="User profile description.")
+    parser.add_argument("--url", type=str, default=0.5, help="VK user URL.")
+    parser.add_argument("--model_type", type=str, default="bert-l", help="Type for matcher model.", required=False)
+    parser.add_argument("--matcher_threshold", type=float, default=0.5, help="Matcher threshold.", required=False)
     args = parser.parse_args()
 
     # parse vk
     parsed_posts = analyze_vk_posts(
-        profile_url="https://vk.com/vandarkh0lme",
+        profile_url=args.url,
         depth=1,
         period_days=30
     )
-    
     # call llm
     llm_results = []
     for parsed_post in parsed_posts:
@@ -29,7 +29,7 @@ def main():
                       profile=args.profile,
                       threshold=args.matcher_threshold,
                       )
-    matcher(llm_results)
+    print(matcher(llm_results))
 
 
 if __name__ == "__main__":
